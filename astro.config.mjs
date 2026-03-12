4e6291eb95721afa7e9fa2e4bbade66a1a2cdc16
CristhianCG <cristhiancg9x@gmail.com>
2026-03-12 16:28:48 -0500

    fix: set official netlify domain for sitemap and canonical URLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://electrion-servicios-online.com',
+  site: 'https://electrion-servicios-online.netlify.app',
   integrations: [sitemap()]
 });
